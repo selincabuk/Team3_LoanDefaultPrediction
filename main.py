3e1309393d51f2d77c3b344cd97638f69f33a8ea
Selin Çabuk <selincbk13@gmail.com>
2024-04-28 18:04:03 +0300

    The first look of the dataset

--- main.py
+++ main.py
@@ -2,4 +2,28 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-print("Hello, World!")
+def read_the_data_file(dataset_file):
+    df = pd.read_csv(dataset_file)
+    return df
+
+
+def main():
+     frame = read_the_data_file('Dataset\Data_Train.csv')
+
+
+
+
+
+def main():
+    
+    frame = read_the_data_file('Dataset/Data_Train.csv')
+
+    print("The first 5 row of the dataset:")
+    print(frame.head())
+
+    
+    print("\nThe size of the dataset:")
+    print(frame.shape)
+
+if __name__ == "__main__":
+    main()
